ac7878e78f956fc33198355e5e5f3311ffd2bc74
FIC <FIC@info.local>
2021-07-05 10:37:12 -0300

    Projeto concluido

--- SP-Medical-Group/SP_Medical_Group.DDL.sql
+++ SP-Medical-Group/SP_Medical_Group.DDL.sql
@@ -31,17 +31,17 @@ GO
 CREATE TABLE usuario
 (
 	idUsuario			INT PRIMARY KEY IDENTITY
-	,idTipoUsuario		INT FOREIGN KEY REFERENCES tipoUsuario
-	,nome				VARCHAR(200) NOT NULL UNIQUE
-	,email				VARCHAR(200) NOT NULL UNIQUE
-	,senha				VARCHAR(50) NOT NULL
+	,idTipoUsuario		INT FOREIGN KEY REFERENCES tipoUsuario(idTipoUsuario)
+	,nome				VARCHAR(200) NOT NULL UNIQUE 
+	,email				VARCHAR(200) NOT NULL UNIQUE 
+	,senha				VARCHAR(50) NOT NULL 
 );
 GO
 
 CREATE TABLE paciente
 (
 	idPaciente			INT PRIMARY KEY IDENTITY
-	,idUsuario			INT FOREIGN KEY REFERENCES usuario
+	,idUsuario			INT FOREIGN KEY REFERENCES usuario(idUsuario)
 	,nomePaciente		VARCHAR(200) NOT NULL UNIQUE
 	,dataNascimento		DATETIME NOT NULL
 	,telefone			VARCHAR(14) 
@@ -55,9 +55,9 @@ CREATE TABLE medico
 (
 	idMedico			INT PRIMARY KEY IDENTITY
 	,nomeMedico				VARCHAR(200) NOT NULL UNIQUE
-	,idUsuario			INT FOREIGN KEY REFERENCES usuario
-	,idEspecialidade	INT FOREIGN KEY REFERENCES especialidade
-	,idClinica			INT FOREIGN KEY REFERENCES clinica
+	,idUsuario			INT FOREIGN KEY REFERENCES usuario(idUsuario)
+	,idEspecialidade	INT FOREIGN KEY REFERENCES especialidade(idEspecialidade)
+	,idClinica			INT FOREIGN KEY REFERENCES clinica(idClinica)
 	,crm				VARCHAR(12) UNIQUE
 );
 GO
@@ -65,8 +65,8 @@ GO
 CREATE TABLE consulta
 (
 	idConsulta			INT PRIMARY KEY IDENTITY
-	,idPaciente			INT FOREIGN KEY REFERENCES paciente
-	,idMedico			INT FOREIGN KEY REFERENCES medico
+	,idPaciente			INT FOREIGN KEY REFERENCES paciente(idPaciente)
+	,idMedico			INT FOREIGN KEY REFERENCES medico(idMedico)
 	,situacao			VARCHAR(20) NOT NULL
 );
 GO
